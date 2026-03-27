53498c4f3d004b1f9985c34f9caa791ea5fd5fb2
Eric P <epas001@hotmail.com>
2026-03-27 18:52:32 -0400

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# A simple hello world demo in Python.
+# EPastore, 03/25/2026
+
+def helloworld():
+    return "Hello World!"
+
+def main():
+    print(helloworld())
+
+if __name__ == "__main__":
+    main()
