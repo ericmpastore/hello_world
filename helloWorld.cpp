dd7f9348d20f1e9bbc1d7556d90f4d32fad903a6
Eric P <epas001@hotmail.com>
2026-03-28 16:56:24 -0400

    Added documentation

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*
+    A simple Hello World! demo in C++.
+    EPastore, 03/28/2026
+*/
+
 int main()
 {
     std::cout << "Hello World!";
--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,9 @@
 class HelloWorld
 {
+    /*
+        A simple Hello World! demo in Java.
+        EPastore, 03/28/2026
+    */
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
