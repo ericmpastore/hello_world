7ffe1903db05efe390f7d6dc06b58487ff29aa74
Eric P <epas001@hotmail.com>
2026-04-19 10:37:55 +0200

    Wrote function for variables

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Test function to show Hello World!
 // EPastore, 04/17/2026
 
@@ -38,6 +40,73 @@ function domWrite()
     console.log("Value of y : " + y)
 }
 
+// Test function for strict mode
+// EPastore, 04/19/2026
+
+function strictMode()
+{
+    var a = 10;
+    var b = 40;
+
+    console.log(`The value of a is ${a} and the value of b is ${b}`)
+
+    var privateVar = "Just a string";
+    var undefinedStr = "An undefined value";
+
+    console.log(`The private string is ${privateVar}.`);
+    console.log(`The undefined string is ${undefinedStr}`);
+}
+
+// Test function for variables
+// EPastore, 04/19/2026
+function varTest()
+{
+    var price1 = 5;
+    var price2 = 10;
+    var total = price1 + price2;
+
+    console.log(`Sum of price1 and price2: ${total}`);
+
+    price1 = 21;
+    price2 = 14;
+    total = price1 + price2;
+
+    console.log(`Updated sum of price1 and price2: ${total}`);
+
+    var price1 = 500;
+    var price2 = 310;
+    var total = price1 + price2;
+
+    console.log(`Sum of re-declared price1 and price2: ${total}`);
+
+    let dividend = 420;
+    let divisor = 20;
+
+    console.log(`The division result is ${dividend/divisor}`)
+
+    dividend = 720;
+    divisor = 30;
+
+    console.log(`The updated division result is ${dividend/divisor}`)
+
+    /*
+    Let does not allow redeclaration
+    let dividend = 420;
+    let divisor = 20;
+
+    console.log(`The division result after redeclaration is ${dividend/divisor}`)
+    */
+
+    const MY_BDAY = "21.11.1983";
+    console.log(`My birthday is: ${MY_BDAY}`);
+
+    // MY_BDAY = "21.11.1985";
+    // console.log(`My birthday is: ${MY_BDAY}`);
+}
+
+
 // helloWorld()
 // docWrite()
-domWrite()
+// domWrite()
+// strictMode()
+varTest()
